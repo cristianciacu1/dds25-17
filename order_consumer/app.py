23968f7e9603fae48faebd879b57280914c745af
Vian <vienelrobotin@gmail.com>
2025-02-24 20:01:44 +0100

    separate api and consumers

---
+++ order_consumer/app.py
@@ -0,0 +1,131 @@
+import logging
+import os
+import atexit
+import random
+import uuid
+from collections import defaultdict
+
+import pika
+
+from msgspec import msgpack, Struct
+from flask import Flask, jsonify, abort, Response
+from utils import create_order_db, get_order_from_db, batch_init_users_db, add_item_db, checkout_db
+
+#insert RabbitMQ consumer
+
+
+DB_ERROR_STR = "DB error"
+REQ_ERROR_STR = "Requests error"
+
+GATEWAY_URL = os.environ['GATEWAY_URL']
+
+
+db: redis.Redis = redis.Redis(host=os.environ['REDIS_HOST'],
+                              port=int(os.environ['REDIS_PORT']),
+                              password=os.environ['REDIS_PASSWORD'],
+                              db=int(os.environ['REDIS_DB']))
+
+
+def close_db_connection():
+    db.close()
+
+
+atexit.register(close_db_connection)
+
+
+class OrderValue(Struct):
+    paid: bool
+    items: list[tuple[str, int]]
+    user_id: str
+    total_cost: int
+
+
+def get_order_from_db(order_id: str) -> OrderValue | None:
+    try:
+        # get serialized data
+        entry: bytes = db.get(order_id)
+    except redis.exceptions.RedisError:
+        return abort(400, DB_ERROR_STR)
+    # deserialize data if it exists else return null
+    entry: OrderValue | None = msgpack.decode(entry, type=OrderValue) if entry else None
+    if entry is None:
+        # if order does not exist in the database; abort
+        abort(400, f"Order: {order_id} not found!")
+    return entry
+
+
+def create_order_db(user_id: str):
+    key = str(uuid.uuid4())
+    value = msgpack.encode(OrderValue(paid=False, items=[], user_id=user_id, total_cost=0))
+    try:
+        db.set(key, value)
+    except redis.exceptions.RedisError:
+        return abort(400, DB_ERROR_STR)
+    return jsonify({'order_id': key})
+
+def batch_init_users_db(n: int, n_items: int, n_users: int, item_price: int):
+
+    n = int(n)
+    n_items = int(n_items)
+    n_users = int(n_users)
+    item_price = int(item_price)
+
+    def generate_entry() -> OrderValue:
+        user_id = random.randint(0, n_users - 1)
+        item1_id = random.randint(0, n_items - 1)
+        item2_id = random.randint(0, n_items - 1)
+        value = OrderValue(paid=False,
+                           items=[(f"{item1_id}", 1), (f"{item2_id}", 1)],
+                           user_id=f"{user_id}",
+                           total_cost=2*item_price)
+        return value
+
+    kv_pairs: dict[str, bytes] = {f"{i}": msgpack.encode(generate_entry())
+                                  for i in range(n)}
+    try:
+        db.mset(kv_pairs)
+    except redis.exceptions.RedisError:
+        return abort(400, DB_ERROR_STR)
+    return jsonify({"msg": "Batch init for orders successful"})
+
+
+def find_order_db(order_id: str):
+    order_entry: OrderValue = get_order_from_db(order_id)
+    return jsonify(
+        {
+            "order_id": order_id,
+            "paid": order_entry.paid,
+            "items": order_entry.items,
+            "user_id": order_entry.user_id,
+            "total_cost": order_entry.total_cost
+        }
+    )
+
+
+def add_item_db(order_id: str, item_id: str, item_json: dict, quantity: int):
+    order_entry: OrderValue = get_order_from_db(order_id)
+    order_entry.items.append((item_id, int(quantity)))
+    order_entry.total_cost += int(quantity) * item_json["price"]
+    try:
+        db.set(order_id, msgpack.encode(order_entry))
+    except redis.exceptions.RedisError:
+        return abort(400, DB_ERROR_STR)
+    return Response(f"Item: {item_id} added to: {order_id} price updated to: {order_entry.total_cost}",
+                    status=200)
+
+
+def checkout(order_id: str, order_entry: OrderValue):
+    try:
+        db.set(order_id, msgpack.encode(order_entry))
+    except redis.exceptions.RedisError:
+        return abort(400, DB_ERROR_STR)
+    app.logger.debug("Checkout successful")
+    return Response("Checkout successful", status=200)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
+else:
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
---
+++ order_consumer/utils.py
@@ -0,0 +1,102 @@
+import logging
+import os
+import atexit
+import random
+import uuid
+from collections import defaultdict
+
+import redis
+import requests
+
+from msgspec import msgpack, Struct
+from flask import Flask, jsonify, abort, Response
+
+
+DB_ERROR_STR = "DB error"
+REQ_ERROR_STR = "Requests error"
+
+GATEWAY_URL = os.environ['GATEWAY_URL']
+
+app = Flask("order-service")
+
+db: redis.Redis = redis.Redis(host=os.environ['REDIS_HOST'],
+                              port=int(os.environ['REDIS_PORT']),
+                              password=os.environ['REDIS_PASSWORD'],
+                              db=int(os.environ['REDIS_DB']))
+
+
+def close_db_connection():
+    db.close()
+
+
+atexit.register(close_db_connection)
+
+
+class OrderValue(Struct):
+    paid: bool
+    items: list[tuple[str, int]]
+    user_id: str
+    total_cost: int
+
+
+def get_order_from_db(order_id: str) -> OrderValue | None:
+    try:
+        # get serialized data
+        entry: bytes = db.get(order_id)
+    except redis.exceptions.RedisError:
+        raise Exception
+    # deserialize data if it exists else return null
+    entry: OrderValue | None = msgpack.decode(entry, type=OrderValue) if entry else None
+    return entry
+
+
+def create_order_db(user_id: str):
+    key = str(uuid.uuid4())
+    value = msgpack.encode(OrderValue(paid=False, items=[], user_id=user_id, total_cost=0))
+    try:
+        db.set(key, value)
+    except redis.exceptions.RedisError:
+        raise Exception
+    return key
+
+def batch_init_users_db(n: int, n_items: int, n_users: int, item_price: int):
+
+    n = int(n)
+    n_items = int(n_items)
+    n_users = int(n_users)
+    item_price = int(item_price)
+
+    def generate_entry() -> OrderValue:
+        user_id = random.randint(0, n_users - 1)
+        item1_id = random.randint(0, n_items - 1)
+        item2_id = random.randint(0, n_items - 1)
+        value = OrderValue(paid=False,
+                           items=[(f"{item1_id}", 1), (f"{item2_id}", 1)],
+                           user_id=f"{user_id}",
+                           total_cost=2*item_price)
+        return value
+
+    kv_pairs: dict[str, bytes] = {f"{i}": msgpack.encode(generate_entry())
+                                  for i in range(n)}
+    try:
+        db.mset(kv_pairs)
+    except redis.exceptions.RedisError:
+        raise Exception
+
+
+def find_order_db(order_id: str):
+    return get_order_from_db(order_id)
+
+
+def add_item_db(order_id: str, order_entry: OrderValue):
+    try:
+        db.set(order_id, msgpack.encode(order_entry))
+    except redis.exceptions.RedisError:
+        raise Exception
+
+
+def checkout_db(order_id: str, order_entry: OrderValue):
+    try:
+        db.set(order_id, msgpack.encode(order_entry))
+    except redis.exceptions.RedisError:
+        raise Exception
